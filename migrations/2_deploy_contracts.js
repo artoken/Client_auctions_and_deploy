d29d4b68b31e1924480499dc75f1315437deedc4
CousinAvi <Washington_Capitals_1@mail.ru>
2021-11-11 13:08:31 +1000

    Add deploy Diamond contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-var ART_TOKEN = artifacts.require("./ART_CONTRACT.sol");
+var Diamond = artifacts.require("./Diamond.sol");
 var AuctionBox = artifacts.require("./AuctionBox.sol");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(ART_TOKEN);
+  await deployer.deploy(Diamond);
   await deployer.deploy(AuctionBox);
   //token = await Token.deployed();
   //await token.passMinterRole(dBank.address);
